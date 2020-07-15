99afa18ffe3886cf0c7655ba73dcf9c14d1bbbee
Pranav Rayudu <tarakapranav@gmail.com>
2020-07-15 12:03:10 -0500

    style update

--- src/components/grid/MainGridContent.js
+++ src/components/grid/MainGridContent.js
@@ -2,7 +2,7 @@ import React from "react"
 import contentStyles from "./grid.module.scss"
 
 export default props => (
-  <div className={[contentStyles.content, props.className].join(" ")}>
+  <div className={[contentStyles.content, props.className].join(" ")} style={props.style}>
     {props.children}
   </div>
 )
